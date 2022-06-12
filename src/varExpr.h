96d7e8919d8740eb52c8ddeee5bb7a032583c389
cima22 <92120560+cima22@users.noreply.github.com>
2022-06-12 17:01:11 +0200

    Update varExpr.h

--- src/varExpr.h
+++ src/varExpr.h
@@ -3,3 +3,13 @@ class VarExpr : public Expr{
 	public:
 		VarExpr(const Var& v);
 };
+
+class ConstExpr : public Expr{
+	public:
+		ConstExpr(const int i);
+};
+
+class CompExpr : public Expr{
+	public:
+		CompExpr(const Expr& e1, const Expr& e2);
+}
