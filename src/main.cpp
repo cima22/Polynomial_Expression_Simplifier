219318bb0061ae8f9b2e350f3afeca37d57fd8f9
cima22 <92120560+cima22@users.noreply.github.com>
2022-06-13 15:13:01 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,7 @@ int main(int argc, char* argv[]){
 	Var y{"y",3};
 	Var z{"z",4};
 	
-	Expr& ex = x + 2;
-	Expr& ex2 = 2 * y;
-	Expr& ex3 = 10 - z;
-	Expr& ex4 = x * z;
-	std::cout << ex << "\n" << ex2 << "\n" << ex3 << "\n" << ex4 << std::endl;
+	Expr& ex = x + 1 + 2;
+	Expr& ex1 = 2 * (y + z);
+	std::cout << ex << "\n" << ex1 << std::endl;
 }
