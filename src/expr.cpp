13800969ce8744dedc7b7da6d148af74c6c85341
cima22 <92120560+cima22@users.noreply.github.com>
2022-06-18 12:56:15 +0200

    Update expr.cpp

--- src/expr.cpp
+++ src/expr.cpp
@@ -1,1 +1,64 @@
-jdnf
+#include "expr.h"
+
+Expr::Expr(ParentExpr * ptr):
+obj{ptr}{}
+
+ostream& operator<<(ostream& os, const Expr& ex){
+ os << *obj;
+  return os;
+}
+
+const Expr& operator+ (const Var& v, int c){
+	VarExpr*   v_e = new VarExpr{v};
+	ConstExpr* c_e = new ConstExpr{c};
+  CompExpr* comp = new CompExpr{*v_e,*c_e,operation::sum};
+	return Expr{comp};
+}
+
+const Expr& operator+ (int c, const Var& v){
+	VarExpr*   v_e = new VarExpr{v};
+	ConstExpr* c_e = new ConstExpr{c};
+  CompExpr* comp = new CompExpr{*c_e,*v_e,operation::sum};
+  return Expr{comp};
+}
+const Expr& operator- (const Var& v, int c){
+	VarExpr*   v_e = new VarExpr{v};
+	ConstExpr* c_e = new ConstExpr{c};
+	CompExpr* comp = new CompExpr{*v_e,*c_e,operation::sub};
+  return Expr{comp};
+}
+const Expr& operator- (int c, const Var& v){
+	VarExpr* v_e   = new VarExpr{v};
+	ConstExpr* c_e = new ConstExpr{c};
+	CompExpr* comp = new CompExpr{*c_e,*v_e,operation::sub};
+  return Expr{comp};
+}
+const Expr& operator* (const Var& v, int c){
+	VarExpr* v_e    = new VarExpr{v};
+	ConstExpr* c_e  = new ConstExpr{c};
+	ConstExpr* comp = new CompExpr{*v_e,*c_e,operation::mul};
+  return Expr{comp};
+}
+const Expr& operator* (int c, const Var& v){
+	VarExpr*   v_e = new VarExpr{v};
+	ConstExpr* c_e = new ConstExpr{c};
+	CompExpr* comp = new CompExpr{*c_e,*v_e,operation::mul};
+  return Expr{comp};
+}
+const Expr& operator+ (const Var& v1, const Var& v2){
+	VarExpr* v_e_1 = new VarExpr{v1};
+	VarExpr* v_e_2 = new VarExpr{v2};
+	CompExpr* comp = new CompExpr{*v_e_1,*v_e_2,operation::sum};
+  return Expr{comp};
+}
+const Expr& operator- (const Var& v1, const Var& v2){
+	VarExpr* v_e_1 = new VarExpr{v1};
+	VarExpr* v_e_2 = new VarExpr{v2};
+	CompExpr* comp = new CompExpr{*v_e_1,*v_e_2,operation::sub};
+  return Expr{comp};
+}
+const Expr& operator* (const Var& v1, const Var& v2){
+	VarExpr* v_e_1 = new VarExpr{v1};
+	VarExpr* v_e_2 = new VarExpr{v2};
+	CompExpr* comp = new CompExpr{*v_e_1,*v_e_2,operation::mul};
+}
