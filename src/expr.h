adf21f50d43725ba23ea7a66cfcca9af9c61ef22
cima22 <92120560+cima22@users.noreply.github.com>
2022-06-18 16:25:33 +0200

    Update expr.h

--- src/expr.h
+++ src/expr.h
@@ -14,7 +14,28 @@ class Expr{
 		bool equivalent(const Expr& e1, const Expr& e2);
 
 		friend std::ostream& operator<<(std::ostream& os, const Expr& ex);
+		
+		friend const Expr operator+ (const CompExpr& e1, const CompExpr& e2);
+		friend const Expr operator- (const CompExpr& e1, const CompExpr& e2);
+		friend const Expr operator* (const CompExpr& e1, const CompExpr& e2);
 
+		friend const Expr operator+ (const Expr& e1, int i);
+		friend const Expr operator+ (int i, const Expr& e1);
+
+		friend const Expr operator- (const Expr& e1, int i);
+		friend const Expr operator- (int i, const Expr& e1);
+		
+		friend const Expr operator* (const Expr& e1, int i);
+		friend const Expr operator* (int i, const Expr& e1);
+
+		friend const Expr operator+ (const Expr& e1, const Var& v1);
+		friend const Expr operator+ (const Var& v1, const Expr& e1);
+
+		friend const Expr operator- (const Expr& e1, const Var& v1);
+		friend const Expr operator- (const Var& v1, const Expr& e1);
+
+		friend const Expr operator* (const Expr& e1, const Var& v1);
+		friend const Expr operator* (const Var& v1, const Expr& e1);
 };
 
 const Expr operator+ (const Var& v, int c);
