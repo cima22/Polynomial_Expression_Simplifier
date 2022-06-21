6c6ea24c22bfc08cefa108a533f537cabec492e3
cima22 <gabri@DESKTOP-U95MR02.localdomain>
2022-06-21 12:41:02 +0200

    jvb

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]){
 	CompExpr ex = (2 * x * y * 3 * x) + x + 4 + x * x;
 	std::map<unsigned int,const ParentExpr*> m{};
 	m = ex.get_coeffs(y);
-\	for(auto& e : m)
+	for(auto& e : m)
 		std::cout << e.first << " : " << *e.second << "\n";
 
 }
